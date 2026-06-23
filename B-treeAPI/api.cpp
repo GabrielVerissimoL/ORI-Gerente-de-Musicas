ce89dd2b3effc175d76f1b8bb50135d70264cfd9
tomas-amasson <massontomas18@gmail.com>
2026-06-22 22:00:57 -0300

    outras funcionalidades

--- B-treeAPI/api.cpp
+++ B-treeAPI/api.cpp
@@ -1,123 +1,274 @@
 #include <drogon/HttpController.h>
 #include <map>
 #include <string>
-#include "../MusicClass/music.hpp"
+#include <vector>
 #include "../B-treeClass/btree.hpp"
+#include "../MusicClass/music.hpp"
+#include "../databaseClass/database.hpp"
 
 using namespace drogon;
 
 BTree mainTree;
 
+void populate_json(Json::Value *ret, music current);
 
-
-
-// Controller de alto nível para gerenciar produtos
 class MusicController : public HttpController<MusicController> {
 public:
 
     // Definição das rotas usando macros de alto nível
     METHOD_LIST_BEGIN
         ADD_METHOD_TO(MusicController::SEARCH_BY_ID, "/music/{id}", Get);
+	ADD_METHOD_TO(MusicController::SEARCH_BY_NAME, "/music/{name}", Get);
+	ADD_METHOD_TO(MusicController::SEARCH_BY_SINGER, "/music/{singer}", Get);
+	ADD_METHOD_TO(MusicController::SEARCH_BY_ALBUM, "/music/{album}", Get);
+//	ADD_METHOD_TO(MusicController::SEARCH_BY_POPUL, "/music/{popularity}", Get);
+//	ADD_METHOD_TO(MusicController::DELETE, "/music/{id}", Delete);
         ADD_METHOD_TO(MusicController::UPLOAD, "/music", Post);
     METHOD_LIST_END
-
-    void SEARCH_BY_ID(const HttpRequestPtr& req, std::function<void(const HttpResponsePtr&)>&& callback, int id) {
-
-	Json::Value ERR;
-	ERR["Erro"] = "Essa musica não existe.";
-	auto res_ERR = HttpResponse::newHttpJsonResponse(ERR);
-	res_ERR->setStatusCode(k404NotFound);
 	
-	BTree bt;
-	BTreeNode *found = bt.search(id);	
+    	void SEARCH_BY_ID(const HttpRequestPtr& req, std::function<void(const HttpResponsePtr&)>&& callback, int id) 
+    	{
 
-	if (found == NULL || &found->keys[id] == NULL)
+		Json::Value ERR;
+		ERR["Erro"] = "Essa musica não existe.";
+		auto res_ERR = HttpResponse::newHttpJsonResponse(ERR);
+		res_ERR->setStatusCode(k404NotFound);
+
+		BTree bt;
+		BTreeNode *found = bt.search(id);
+
+		printf("%p\n", found);
+		if (found == nullptr)
+		{
+			callback(res_ERR);
+			return ;
+		}
+	
+		for (int i = 0; i < found->n; i++)
+		{
+			if (ORDER > i && &found->keys[i] == nullptr)
+				continue ;
+
+			std::cout << found->keys[i].getName() << std::endl;
+		}	
+
+		if (found == NULL || &found->keys[id] == NULL)
+		{
+			callback(res_ERR);
+			return ;
+		}
+	
+		int rrn = found->keys[id].getrrn();
+	        if (rrn < 0) 
+		{
+			ERR["Erro"] = "RRN invalido.";
+			res_ERR = HttpResponse::newHttpJsonResponse(ERR);
+        		callback(res_ERR);
+        	   	return;
+        	}
+
+		FILE *dataout = fopen(FILE_NAME, "rb");
+		if (dataout == nullptr)
+		{
+			ERR["Erro"] = "Não foi possivel abrir o arquivo de dados.";
+			res_ERR = HttpResponse::newHttpJsonResponse(ERR);
+			callback(res_ERR);
+			return ;
+		}
+
+		music *ptr;
+		ptr->setrrn(rrn);
+
+		fseek(dataout, ptr->getrrn(), SEEK_SET);
+
+		if (fread(ptr, sizeof(music), 1, dataout) == 0)
+		{
+			fclose(dataout);
+			ERR["Erro"] = "Não foi possivel ler do arquivo de dados.";
+			res_ERR = HttpResponse::newHttpJsonResponse(ERR);
+			callback(res_ERR);
+			return ;
+		}
+		
+
+
+
+        	Json::Value Answer;
+		populate_json(&Answer, *ptr);
+
+		/*
+		Answer["Name"] 		= ptr->getName();
+		Answer["Singer"] 	= ptr->getSinger();
+		Answer["Album Name"] 	= ptr->getalbum_name();
+		Answer["URL"] 		= ptr->getUrl();
+		Answer["Genre"]		= ptr->getGenre();
+		Answer["Duration"]	= ptr->getDuration();
+		Answer["Popularity"]	= ptr->getPopularity();
+		*/
+
+		fclose(dataout);
+
+       	 	auto res = HttpResponse::newHttpJsonResponse(Answer);
+	        callback(res);
+
+		return ;
+    }
+
+
+    	void SEARCH_BY_NAME(const HttpRequestPtr& req, std::function<void(const HttpResponsePtr&)>&& callback, std::string name) 
 	{
-		callback(res_ERR);
+	
+		database db;
+		std::vector<music> msc = db.NameSearch(name);
+		Json::Value Answer(Json::arrayValue);
+		Json::Value Musics;
+	
+
+		for (int i = 0; i < msc.size(); i++)
+		{
+			populate_json(&Musics, msc[i]);
+			Answer.append(Musics);
+		}
+
+		auto res = HttpResponse::newHttpJsonResponse(Answer); 
+		callback(res);
+		return ;
+	}
+
+
+	void SEARCH_BY_SINGER(const HttpRequestPtr& req, std::function<void(const HttpResponsePtr&)>&& callback, std::string singer) 
+	{
+
+		database db;
+		std::vector<music> msc = db.SingerSearch(singer);
+		Json::Value Answer(Json::arrayValue);
+		Json::Value Musics;
+	
+		for (int i = 0; i < msc.size(); i++)
+		{
+			populate_json(&Musics, msc[i]);
+			Answer.append(Musics);
+		}
+
+	
+		auto res = HttpResponse::newHttpJsonResponse(Answer); 
+		callback(res);
+		return ;
+    	}
+
+    	void SEARCH_BY_GENRE(const HttpRequestPtr& req, std::function<void(const HttpResponsePtr&)>&& callback, std::string genre) 
+	{
+		
+		database db;
+		std::vector<music> msc = db.GenreSearch(genre);
+		Json::Value Answer(Json::arrayValue);
+		Json::Value Musics;
+	
+		for (int i = 0; i < msc.size(); i++)
+		{
+			populate_json(&Musics, msc[i]);
+			Answer.append(Musics);
+		}
+
+	
+		auto res = HttpResponse::newHttpJsonResponse(Answer); 
+		callback(res);
 		return ;
 	}
 	
-	int rrn = found->keys[id].getrrn();
-        if (rrn < 0) {
-        	callback(res_ERR);
-           	return;
-        }
 
-	FILE *dataout = fopen(FILE_NAME, "rb");
-	if (dataout == nullptr)
+    	void SEARCH_BY_ALBUM(const HttpRequestPtr& req, std::function<void(const HttpResponsePtr&)>&& callback, std::string album) 
 	{
-		callback(res_ERR);
+		
+		database db;
+		std::vector<music> msc = db.Album_nameSearch(album);
+		Json::Value Answer(Json::arrayValue);
+		Json::Value Musics;
+	
+		for (int i = 0; i < msc.size(); i++)
+		{
+			populate_json(&Musics, msc[i]);
+			Answer.append(Musics);
+		}
+
+	
+		auto res = HttpResponse::newHttpJsonResponse(Answer); 
+		callback(res);
 		return ;
 	}
 
-	music *ptr;
-	ptr->setrrn(rrn);
-
-	fseek(dataout, ptr->getrrn(), SEEK_SET);
-
-	if (fread(ptr, sizeof(music), 1, dataout) == 0)
+    	void UPLOAD(const HttpRequestPtr& req, std::function<void(const HttpResponsePtr&)>&& callback) 
 	{
-		callback(res_ERR);
-	}
-
-        Json::Value Answer;
-	Answer["Name"] 		= ptr->getName();
-	Answer["Singer"] 	= ptr->getSinger();
-	Answer["Album Name"] 	= ptr->getalbum_name();
-	Answer["URL"] 		= ptr->getUrl();
-	Answer["Genre"]		= ptr->getGenre();
-	Answer["Duration"]	= ptr->getDuration();
-	Answer["Popularity"]	= ptr->getPopularity();
-
-	fclose(dataout);
-
-        auto res = HttpResponse::newHttpJsonResponse(Answer);
-        callback(res);
-    }
-
-    // 3. POST - Criar produto
-    void UPLOAD(const HttpRequestPtr& req, std::function<void(const HttpResponsePtr&)>&& callback) {
 	
-        auto res_ERR = HttpResponse::newHttpResponse();
-        res_ERR->setStatusCode(k400BadRequest);
+        	auto res_ERR = HttpResponse::newHttpResponse();
+        	res_ERR->setStatusCode(k400BadRequest);
 
-        auto Music_json = req->getJsonObject();
-        if (!Music_json) {
-            callback(res_ERR);
-            return;
-        }
+	        auto Music_json = req->getJsonObject();
+        	if (!Music_json) 
+		{
+	            callback(res_ERR);
+        	    return;
+        	}
 
 
-	std::string name 	= (*Music_json)["name"].asString();
-	std::string singer 	= (*Music_json)["singer"].asString();
-	std::string album 	= (*Music_json)["album_name"].asString();
-	std::string url		= (*Music_json)["url"].asString();
-	std::string genre	= (*Music_json)["genre"].asString();
-	float duration		= (*Music_json)["duration_ms"].asFloat();
-	float popularity	= (*Music_json)["popularity"].asFloat();
-	int album_id		= (*Music_json)["album_id"].asInt();
-        int id 			= (*Music_json)["id"].asInt();
-	int rrn 		= (*Music_json)["rrn"].asInt(); 
+		std::string name 	= (*Music_json)["name"].asString();
+		std::string singer 	= (*Music_json)["singer"].asString();
+		std::string album 	= (*Music_json)["album_name"].asString();
+		std::string url		= (*Music_json)["url"].asString();
+		std::string genre	= (*Music_json)["genre"].asString();
+		float duration		= (*Music_json)["duration_ms"].asFloat();
+		float popularity	= (*Music_json)["popularity"].asFloat();
+		int album_id		= (*Music_json)["album_id"].asInt();
+	        int id 			= (*Music_json)["id"].asInt();
+		int rrn 		= (*Music_json)["rrn"].asInt(); 
 	
-	// objeto music criado
-	music k(name, singer, album, url, genre, duration, popularity, album_id, id, rrn);
+		// objeto music criado
+		music k(name, singer, album, url, genre, duration, popularity, album_id, id, rrn);
 
-	mainTree.insert(k);
+		mainTree.insert(k);
 
-        Json::Value sucesso;
-        sucesso["mensagem"] = "Musica adicionado.";
-        
-        auto res = HttpResponse::newHttpJsonResponse(sucesso);
-        res->setStatusCode(k201Created);
-        callback(res);
-    }
+
+		FILE *datain = fopen(FILE_NAME, "ab+");
+		if (fwrite(&k, sizeof(music), 1, datain) == 0)
+		{
+			Json::Value erro;
+			erro["Erro"] = "Não foi possivel adicionar essa musica.";
+
+			auto res = HttpResponse::newHttpJsonResponse(erro);
+			callback(res);
+			return ;	
+		}
+
+	        Json::Value sucesso;
+        	sucesso["mensagem"] = "Musica adicionado.";
+       
+
+	        auto res = HttpResponse::newHttpJsonResponse(sucesso);
+        	res->setStatusCode(k201Created);
+	        callback(res);
+		return ;
+	    }
 };
+
+void populate_json(Json::Value *ret, music current)
+{	
+
+	(*ret)["Name"] 		= current.getName();
+	(*ret)["Singer"] 	= current.getSinger();
+	(*ret)["Album"]		= current.getalbum_name();
+	(*ret)["Genre"]		= current.getGenre();
+	(*ret)["URL"]		= current.getUrl();
+	(*ret)["Popularity"]	= current.getPopularity();
+	(*ret)["Duration"]	= current.getDuration();
+
+	return ;
+}
 
 int main() {
 
 	app()
         .addListener("0.0.0.0", 5000)
-        .setThreadNum(4) 
+        .setThreadNum(1) 
         .run();
     return 0;
 }
